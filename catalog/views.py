4376e531a12cb2da5efeb47f0d1b5c0a1334242a
Alesh Houdek <mail@alesh.com>
2019-04-17 07:16:42 -0600

    Added session visit counter

--- catalog/views.py
+++ catalog/views.py
@@ -9,11 +9,15 @@ def index(request):
   num_instances_available = BookInstance.objects.filter(status__exact='a').count()
   num_authors = Author.objects.count()
 
+  num_visits = request.session.get('num_visits', 0)
+  request.session['num_visits'] = num_visits + 1
+
   context = {
     'num_books': num_books,
     'num_instances': num_instances,
     'num_instances_available': num_instances_available,
     'num_authors': num_authors,
+    'num_visits': num_visits,
   }
 
   return render(request, 'index.html', context=context)
