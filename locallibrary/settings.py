b6004a279088754043289c1f4d1593c1d285ed5d
Alesh Houdek <mail@alesh.com>
2019-04-22 06:43:52 -0600

    Added authentication routes and templates

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
